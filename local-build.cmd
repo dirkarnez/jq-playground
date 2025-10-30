a259960574bed658cff6f4e121efe325ea610cc0
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-31 01:19:12 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,1 +1,14 @@
+@REM run as Administrator
+@echo off
 
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set SEVENZIP=C:\"Program Files"\7-Zip\7z.exe
+
+set JQ_DIR=%DOWNLOADS_DIR%
+set JQ_EXE=%JQ_DIR%\jq-windows-amd64.exe
+
+set PATH=^
+%JQ_DIR%;
+
+%JQ_EXE% 
