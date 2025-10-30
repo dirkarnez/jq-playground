a5c711eddd620ab111f2f2a5557135710017a453
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-31 01:18:21 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,1 +1,17 @@
+@REM run as Administrator
+@echo off
 
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set SEVENZIP=C:\"Program Files"\7-Zip\7z.exe
+
+set JQ_DIR=%DOWNLOADS_DIR%
+set JQ_EXE=%JQ_DIR%\jq-windows-amd64.exe
+if not exist %JQ_EXE% (
+cd /d "%JQ_DIR%" &&^
+%SystemRoot%\System32\curl.exe "https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-windows-amd64.exe" -L -O &&^
+)
+
+if exist %JQ_EXE% (
+    echo jq %JQ_EXE% found
+)
